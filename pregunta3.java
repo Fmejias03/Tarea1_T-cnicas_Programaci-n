47b967a7e8862ffdce6ca94e1509b098faedbc1a
Eloy Sánchez Tamayo <113373957+Eloy31st@users.noreply.github.com>
2023-02-02 19:17:51 +0000

    tarea1

--- pregunta3.java
+++ pregunta3.java
@@ -3,19 +3,11 @@
 //codigo en java para el algotitmo de ordenamiento de burbuja de forma recursiva con programacion orientada a objetos
 
 import java.util.Scanner;
-public class Pregunta3{
+public class pregunta3 {
     public static void main(String[] args) {
+        System.out.println("¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?");
         Scanner sc = new Scanner(System.in);
-        int[] arreglo = new int[10];
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.println("Ingrese el numero " + (i+1) + " del arreglo");
-            arreglo[i] = sc.nextInt();
-        }
-        burbuja burbuja = new burbuja(arreglo);
-        burbuja.ordenar();
-        System.out.println("El arreglo ordenado es: ");
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.println(arreglo[i]);
-        }
+        String respuesta = sc.nextLine();
+        System.out.println("Tu respuesta es: " + respuesta);
     }
 }
