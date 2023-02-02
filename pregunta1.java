b869664b44d120d447a666e49a294d972a9c6f3e
Eloy Sánchez Tamayo <113373957+Eloy31st@users.noreply.github.com>
2023-02-02 18:54:53 +0000

    Tarea1

---
+++ pregunta1.java
@@ -0,0 +1,8 @@
+//¿Qué algoritmos usas en tu vida cotidiana?
+
+//¿Piensas que podrías escribir un programa para hacerlos más eficientes?
+
+//Path: pregunta2.java
+
+//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
---
+++ pregunta3.java
@@ -0,0 +1,5 @@
+//¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
+
+//codigo en java para el algotitmo de ordenamiento de burbuja de forma recursiva con programacion orientada a objetos
+
+//codigo java
